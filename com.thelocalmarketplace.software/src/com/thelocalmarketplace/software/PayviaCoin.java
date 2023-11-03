e2f66f30c4de7e0fce9eb2007e79d2cb48e33dca
eliza <eliza@Brad>
2023-11-03 17:18:31 -0600

    Weight descepancy testing and blocking complete moving onto barcode

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayviaCoin.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayviaCoin.java
@@ -2,32 +2,107 @@
 //Justine Mangaliman UCID: 30164741
 package com.thelocalmarketplace.software;
 import java.math.BigDecimal;
+
+import com.tdc.IComponent;
+import com.tdc.IComponentObserver;
 import com.tdc.coin.Coin;
+import com.tdc.coin.CoinSlot;
+import com.tdc.coin.CoinSlotObserver;
+import com.tdc.coin.CoinStorageUnit;
+import com.tdc.coin.CoinStorageUnitObserver;
 import com.thelocalmarketplace.hardware.CoinTray;
 
 
-public class PayviaCoin {
+
+public class PayviaCoin implements CoinStorageUnitObserver {
 	private BigDecimal amount_inserted = BigDecimal.ZERO; //
 	private BigDecimal amount_owed; 
+	private CoinSlot coinSlot;
+	private WeightDiscrepancy discrepancy;
 	private CoinTray dispenced;
 	
 	//inalize value of amount_owed and CoinTray
-	public PayviaCoin(BigDecimal total, CoinTray tray ) {
+	public PayviaCoin(BigDecimal total, CoinTray tray,  WeightDiscrepancy new_discrepancy, CoinSlot new_coinSlot) {
 		amount_owed = total;
 		dispenced = tray;
+		this.discrepancy = new_discrepancy;
+		this.coinSlot = new_coinSlot;
+		
+//		observer.attach(this);
+		
+		
 	}
 	
 	
 	public boolean MakePayment(Coin money) {
 		amount_inserted = amount_inserted.add(money.getValue());
-		if (amount_inserted.compareTo(amount_owed)<0){
-			return true;
-			
-		}
-		return false;
-		 
 		
-	}
-	
+		// Check weight discrepancy
+		enabled(coinSlot);
+		if(!discrepancy.CompareWeight()) {
+			disabled(coinSlot);
+		} else if (amount_inserted.compareTo(amount_owed)<0){
+				return true;
+		}
+		
+		return false;
 
 }
+
+
+	@Override
+	public void enabled(IComponent<? extends IComponentObserver> component) {
+		// TODO Auto-generated method stub
+		component.enable();
+	}
+
+
+	@Override
+	public void disabled(IComponent<? extends IComponentObserver> component) {
+		// TODO Auto-generated method stub
+		component.disable();
+	}
+
+
+	@Override
+	public void turnedOn(IComponent<? extends IComponentObserver> component) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void turnedOff(IComponent<? extends IComponentObserver> component) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void coinsFull(CoinStorageUnit unit) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void coinAdded(CoinStorageUnit unit) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void coinsLoaded(CoinStorageUnit unit) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void coinsUnloaded(CoinStorageUnit unit) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
