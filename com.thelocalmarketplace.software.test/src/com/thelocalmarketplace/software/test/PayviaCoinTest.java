796d2984c3b3e49b90f5ebfb5912fc3fb8f40320
eliza <eliza@Brad.ucalgary.ca>
2023-11-03 17:19:31 -0600

    Disabling complete for paywithcoing for weight descrapncy. Test updated

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/PayviaCoinTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/PayviaCoinTest.java
@@ -1,9 +1,16 @@
 //Elizabeth Szentmiklossy UCID:30165216
 // Justine Mangaliman UCID: 30164741
 package com.thelocalmarketplace.software.test;
+import com.jjjwelectronics.Mass;
+import com.jjjwelectronics.scale.AbstractElectronicScale;
+import com.jjjwelectronics.scale.ElectronicScale;
 import com.tdc.coin.Coin;
+import com.tdc.coin.CoinSlot;
 import com.thelocalmarketplace.hardware.CoinTray;
 import com.thelocalmarketplace.software.*;
+
+import powerutility.PowerGrid;
+
 import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
@@ -13,14 +20,22 @@ import java.util.Locale;
 import org.junit.Test;
 
 public class PayviaCoinTest {
-	
+		ElectronicScale listner = new ElectronicScale();
+		WeightDiscrepancy discrepancy = new WeightDiscrepancy(Mass.ZERO, listner);
+		CoinSlot coin_slot = new CoinSlot();
+		
+		PowerGrid grid = PowerGrid.instance();
 	
 
 	    @Test
 	    public void AddCoins() {
+	    	coin_slot.connect(grid);
+	    	coin_slot.activate();
+	    	
+	    	
 	    	Coin.DEFAULT_CURRENCY = Currency.getInstance(Locale.CANADA); 
-	    	PayviaCoin paymentHandeler = new PayviaCoin(BigDecimal.ZERO, null);
-	    	PayviaCoin payment = new PayviaCoin(BigDecimal.valueOf(3), null);
+	    	PayviaCoin paymentHandeler = new PayviaCoin(BigDecimal.ZERO, null, discrepancy, coin_slot);
+	    	PayviaCoin payment = new PayviaCoin(BigDecimal.valueOf(3), null, discrepancy, coin_slot);
 	    	
 	    	Coin tray = new Coin(BigDecimal.ONE);
 
