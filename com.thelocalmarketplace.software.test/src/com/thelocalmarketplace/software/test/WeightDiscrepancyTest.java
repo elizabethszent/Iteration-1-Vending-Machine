b30e853616a4ab211ab8c938a3f411eba597c379
eliza <eliza@Brad>
2023-11-04 15:25:29 -0600

    Updated comments and JavaDoking

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/WeightDiscrepancyTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/WeightDiscrepancyTest.java
@@ -1,3 +1,8 @@
+//Elizabeth Szentmiklossy UCID: 30165216
+//Justine Mangaliman UCID: 30164741
+//Enzo Mutiso UCID: 30182555
+//Abdelrahman Mohamed UCID: 30162037
+//Mohammad Mustafa Mehtab UCID: 30189394
 package com.thelocalmarketplace.software.test;
 
 import static org.junit.Assert.assertEquals;
@@ -22,39 +27,48 @@ import com.thelocalmarketplace.software.WeightDiscrepancy;
 
 import powerutility.PowerGrid;
 
+/**
+ * This class contains JUnit test cases for the WeightDiscrepancy class to verify its functionality.
+ * @author Elizabeth Szentmiklossy (UCID: 30165216)
+ */
 public class WeightDiscrepancyTest {
 	
 		PowerGrid grid = PowerGrid.instance();
 		Numeral[] numerals = new Numeral[]{Numeral.valueOf((byte) 2)};
 		
 		ElectronicScale listner = new ElectronicScale();
-		
+		// Create an instance of WeightDiscrepancy with an expected weight of one gram and the electronic scale listener.
 		WeightDiscrepancy discrepancy = new WeightDiscrepancy(Mass.ONE_GRAM, listner);
 		Barcode barcode = new Barcode(numerals);
 		BarcodedItem item;
 		
 
-
+		/**
+	     * Test case to check if the actual weight is equal to the expected weight.
+	     */
 	    @Test
 	    public void EqualTO() {
 	    	listner.plugIn(grid);
 	    	listner.turnOn();
-	    	
+	    	// Create a BarcodedItem with the specified barcode and one gram of weight.
 	    	item = new BarcodedItem(barcode, Mass.ONE_GRAM);
 	    	listner.addAnItem(item);
-	    	
+	    	// Verify that the expected and actual weights are equal.
 	        assertEquals(discrepancy.CompareWeight(),true);
 	    }
+	    
+	    /**
+	     * Test case to check if the actual weight is more than the expected weight.
+	     */
 	    @Test
 	    public void MoreThan() {
 	    	listner.plugIn(grid);
 	    	listner.turnOn();
-	    	
 	    	Mass mass = new Mass(20);
-	    	
+	    	// Create a BarcodedItem with the specified barcode and a weight of 20 grams.
 	    	item = new BarcodedItem(barcode, mass);
 	    	listner.addAnItem(item);
-	    	
+	    	// Verify that the expected and actual weights are not equal.
 	        assertEquals(discrepancy.CompareWeight(),false);   	
 			
 		
--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayviaCoin.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayviaCoin.java
@@ -1,6 +1,8 @@
 //Elizabeth Szentmiklossy UCID: 30165216
 //Justine Mangaliman UCID: 30164741
+//Enzo Mutiso UCID: 30182555
 //Abdelrahman Mohamed UCID: 30162037
+//Mohammad Mustafa Mehtab UCID: 30189394
 package com.thelocalmarketplace.software;
 import java.math.BigDecimal;
 
@@ -12,8 +14,13 @@ import com.tdc.coin.CoinSlotObserver;
 import com.tdc.coin.CoinStorageUnit;
 import com.tdc.coin.CoinStorageUnitObserver;
 import com.thelocalmarketplace.hardware.CoinTray;
-
-
+/**
+ * PayviaCoin class handles payments and monitors the payment process, including weight discrepancies.
+ * This class does not yet handle change.
+ *
+ * @author Elizabeth Szentmiklossy (UCID: 30165216)
+ 
+ */
 
 public class PayviaCoin implements CoinStorageUnitObserver {
 	private BigDecimal amount_inserted = BigDecimal.ZERO; //
@@ -22,19 +29,28 @@ public class PayviaCoin implements CoinStorageUnitObserver {
 	private WeightDiscrepancy discrepancy;
 	private CoinTray dispenced;
 	
-	//inalize value of amount_owed and CoinTray
+	/**
+     * Constructor for PayviaCoin class.
+     *
+     * @param total         The total amount owed for the payment.
+     * @param tray          The CoinTray where dispensed coins are collected. (implemented for future iterations)
+     * @param new_discrepancy The WeightDiscrepancy instance to check weight discrepancies.
+     * @param new_coinSlot  The CoinSlot for inserting coins.
+     */
 	public PayviaCoin(BigDecimal total, CoinTray tray,  WeightDiscrepancy new_discrepancy, CoinSlot new_coinSlot) {
 		amount_owed = total;
 		dispenced = tray;
 		this.discrepancy = new_discrepancy;
 		this.coinSlot = new_coinSlot;
 		
-//		observer.attach(this);
-		
-		
 	}
 	
-	
+	/**
+     * Make a payment using a coin.
+     *
+     * @param money The coin used for payment.
+     * @return True if payment is successful, false otherwise.
+     */
 	public boolean MakePayment(Coin money) {
 		amount_inserted = amount_inserted.add(money.getValue());
 		
@@ -51,59 +67,38 @@ public class PayviaCoin implements CoinStorageUnitObserver {
 }
 
 
+	// to enable the coinslot.
 	@Override
 	public void enabled(IComponent<? extends IComponentObserver> component) {
 		// TODO Auto-generated method stub
 		component.enable();
 	}
 
-
+	// to disable the coinslot.
 	@Override
 	public void disabled(IComponent<? extends IComponentObserver> component) {
 		// TODO Auto-generated method stub
 		component.disable();
 	}
 
-
+	// Other overridden methods from CoinStorageUnitObserver (unimplemented).
 	@Override
-	public void turnedOn(IComponent<? extends IComponentObserver> component) {
-		// TODO Auto-generated method stub
-		
+	public void turnedOn(IComponent<? extends IComponentObserver> component) {	
 	}
-
-
 	@Override
 	public void turnedOff(IComponent<? extends IComponentObserver> component) {
-		// TODO Auto-generated method stub
-		
 	}
-
-
 	@Override
-	public void coinsFull(CoinStorageUnit unit) {
-		// TODO Auto-generated method stub
-		
+	public void coinsFull(CoinStorageUnit unit) {	
 	}
-
-
 	@Override
-	public void coinAdded(CoinStorageUnit unit) {
-		// TODO Auto-generated method stub
-		
+	public void coinAdded(CoinStorageUnit unit) {	
 	}
-
-
 	@Override
 	public void coinsLoaded(CoinStorageUnit unit) {
-		// TODO Auto-generated method stub
-		
 	}
-
-
 	@Override
-	public void coinsUnloaded(CoinStorageUnit unit) {
-		// TODO Auto-generated method stub
-		
+	public void coinsUnloaded(CoinStorageUnit unit) {	
 	}
 }
 
