094c5cde09b7f25628cfb1476002c31703244c38
eliza <eliza@Brad>
2023-11-04 23:12:28 -0600

    Final version.Checked and revised

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/StartSessionTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/StartSessionTest.java
@@ -1,3 +1,5 @@
+package com.thelocalmarketplace.software.test;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -5,36 +7,51 @@ import com.thelocalmarketplace.software.StartSession;
 
 import static org.junit.Assert.*;
 
+/**
+ * This class contains unit tests for the StartSession class.
+ */
 public class StartSessionTest {
 
-    private StartSession selfCheckoutControl;
+    private StartSession startSession;
 
     @Before
     public void setUp() {
-        StartSession startSession = new StartSession();
+    	 /**
+         * Sets up the test by creating an instance of the StartSession class.
+         */
+        startSession = new StartSession();
     }
 
     @Test
     public void testSessionNotStartedInitially() {
-        // Assert that the session is not started initially
-        assertFalse("Session should not be started initially", selfCheckoutControl.checkSessionStarted());
+    	 // Assert that the session is not started initially
+        /**
+         * Tests that the session is not started initially and expects the result to be false.
+         */
+        assertFalse("Session should not be started initially", startSession.checkSessionStarted());
     }
 
     @Test
     public void testStartSession() {
         // Call the method to start the session
-        selfCheckoutControl.startSession();
+        startSession.startSession();
         // Assert that the session is started
-        assertTrue("Session should be started after calling startSession", selfCheckoutControl.checkSessionStarted());
+        /**
+         * Tests starting the session and expects the result to be true after calling startSession.
+         */
+        assertTrue("Session should be started after calling startSession", startSession.checkSessionStarted());
     }
 
     @Test
     public void testStartSessionWhenAlreadyStarted() {
         // Start the session for the first time
-        selfCheckoutControl.startSession();
+        startSession.startSession();
         // Try to start the session again
-        selfCheckoutControl.startSession();
-        // The session should remain started (the method does not change session state if it's already started)
-        assertTrue("Session should still be started after calling startSession again", selfCheckoutControl.checkSessionStarted());
+        startSession.startSession();
+     // The session should remain started (the method does not change session state if it's already started)
+        /**
+         * Tests starting the session when it's already started. Expects the session state to remain started.
+         */
+        assertTrue("Session should still be started after calling startSession again", startSession.checkSessionStarted());
     }
 }
--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/StartSession.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/StartSession.java
@@ -5,21 +5,36 @@
 //Mohammad Mustafa Mehtab UCID: 30189394 
 package com.thelocalmarketplace.software;
 
+/**
+ * The StartSession class is responsible for managing a session state and starting a session.
+ * This class provides methods to check if a session has already started and to start a session.
+ * 
+ * @author Justine Mangaliman (UCID: 30164741)
+ *
+ */
+
 public class StartSession {
-	private boolean sessionStarted = false;
+	private boolean status = false;
 	
-	// to check if the session is already started
+	/**
+     * Checks if the session has already started.
+     *
+     * @return {@code true} if the session has started, {@code false} otherwise.
+     */
 	public boolean checkSessionStarted() {
-		if (sessionStarted == false) {
+		if (status == false) {
 			return false;
 		} else {
 			return true;
 		}
 	}
 	
-	// method to start session
+	/**
+     * Starts the session if it hasn't already started.
+     * Prints a message to indicate the result.
+     */
 	public void startSession() {
-		boolean status = checkSessionStarted();
+		status = checkSessionStarted();
 		
 		if (status == false) {
 			status = true;
